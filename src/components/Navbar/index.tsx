1d4651690bccc78ef4fac0ce7008d7f4c02d0589
Masoud <masoudtabatabaei20@gmail.com>
2022-06-25 16:09:16 +0430

    Add main_container class style to main styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./styles.css";
 
-export const Navbar:React.FC = () => {
-  return <nav>
-    <div><a href="#">Github Finder</a></div>
-    <div>
-      <a href="#">Home</a>
-      <a href="#">About</a>
-    </div>
-  </nav>
-}
+export const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <div className="main_container header">
+        <div>
+          <a href="#">Github Finder</a>
+        </div>
+        <div>
+          <a href="#">Home</a>
+          <a href="#" style={{ marginLeft: "30px" }}>
+            About
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Navbar } from "../../components/Navbar";
 
 interface IDashboardProps {
-  children:React.ReactNode;
+  children: React.ReactNode;
 }
 
-export const Dashboard:React.FC<IDashboardProps> = (props) => {
+export const Dashboard: React.FC<IDashboardProps> = (props) => {
   const { children } = props;
 
-  return <div>
-    <Navbar/>
-    {children}
-  </div>
-}
+  return (
+    <div>
+      <Navbar />
+      <div className="main_container">{children}</div>
+    </div>
+  );
+};
