4d06598f03c23070b6eed03422b1a3b55cbf5c4b
Masoud <masoudtabatabaei20@gmail.com>
2022-06-29 22:02:40 +0430

    Fix: Move call api functionality from Search component to parent component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,27 +2,23 @@ import axios from "axios";
 import React, { ChangeEvent, useRef, useState } from "react";
 import "./styles.css";
 
-export const Search: React.FC = () => {
+interface ISearchProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+export const Search: React.FC<ISearchProps> = ({ onSearch }) => {
   const searchTerm = useRef<string>(
     ""
   ) as unknown as React.MutableRefObject<HTMLInputElement>;
 
   const handleSearch = async () => {
     const searchTermInput = searchTerm.current.value.toLowerCase();
-    console.log("search: ", searchTerm.current.value);
     if (searchTermInput === "") {
       console.log("Enter something");
       return;
     }
 
-    try {
-      const result = await axios.get(
-        `https://api.github.com/search/users?q=${searchTermInput}&in=name&type=user`
-      );
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
+    onSearch(searchTermInput);
   };
 
   return (
--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Navbar } from "../../components/Navbar";
 import { Search } from "../../components/Search";
@@ -10,11 +11,24 @@ export const Dashboard: React.FC<IDashboardProps> = (props) => {
   const { children } = props;
   const [users, setUsers] = useState<any>([]);
 
+  const handleSearch = async (searchedTerm: string) => {
+    console.log("search ->", searchedTerm);
+
+    try {
+      const result = await axios.get(
+        `https://api.github.com/search/users?q=${searchedTerm}&in=name&type=user`
+      );
+      console.log(result.data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <div className="main_container">
-        <Search />
+        <Search onSearch={handleSearch} />
         {children}
       </div>
     </div>
