87f38aad500149c3516a8c468abea9397451497c
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-08-06 16:48:27 +0430

    Fix: Modify logic of pagination for the state that number of pages be less than numberOfShownPage

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -48,27 +48,29 @@ export const Pagination: React.FC<IPaginationProps> = ({ total, pageSize, setPag
   const renderButtons = () => {
     let numberOfPages = total % pageSize === 0 ? total / pageSize : Math.floor(total / pageSize) + 1;
     let buttons = [];
-    // for(let i=0 ; i < numberOfPages ; i++) {
-    //   buttons.push(<button key={i} className={(currentPage === i+1) ? "btn active" : "btn"} onClick={() => handlePaginate(i+1)}>{i+1}</button>);
-    // }
-
+  
     buttons.push(<button className="btn" disabled={currentPage === 1} onClick={() => handlePaginate(1)}><FontAwesomeIcon icon={faChevronCircleLeft}></FontAwesomeIcon></button>);
     buttons.push(<button className="btn" disabled={currentPage === 1} onClick={() => handlePaginate(currentPage - 1)}><FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon></button>);
 
-    if (currentPage + numberOfShownPage >= numberOfPages) {
-      for (let i = numberOfPages - numberOfShownPage; i <= numberOfPages ; i++) {
-        buttons.push(<button key={i} className={(currentPage === i) ? "btn active" : "btn"} onClick={() => handlePaginate(i)}>{i}</button>);
+    if(numberOfPages < numberOfShownPage) {
+      for (let i = 0; i < numberOfPages ; i++) {
+        buttons.push(<button key={i} className={(currentPage === i+1) ? "btn active" : "btn"} onClick={() => handlePaginate(i+1)}>{i+1}</button>);
       }
     } else {
-      for (let i = currentPage - 1; i <= currentPage - 1 + numberOfShownPage; i++) {
-        buttons.push(<button key={i} className={(currentPage === i + 1) ? "btn active" : "btn"} onClick={() => handlePaginate(i + 1)}>{i + 1}</button>);
+      if (currentPage + numberOfShownPage >= numberOfPages) {
+        for (let i = numberOfPages - numberOfShownPage; i <= numberOfPages ; i++) {
+          buttons.push(<button key={i} className={(currentPage === i) ? "btn active" : "btn"} onClick={() => handlePaginate(i)}>{i}</button>);
+        }
+      } else {
+        for (let i = currentPage - 1; i <= currentPage - 1 + numberOfShownPage; i++) {
+          buttons.push(<button key={i} className={(currentPage === i + 1) ? "btn active" : "btn"} onClick={() => handlePaginate(i + 1)}>{i + 1}</button>);
+        }
       }
     }
 
     buttons.push(<button className="btn" disabled={currentPage === numberOfPages} onClick={() => handlePaginate(currentPage + 1)}><FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon></button>);
     buttons.push(<button className="btn" disabled={currentPage === numberOfPages} onClick={() => handlePaginate(numberOfPages)}><FontAwesomeIcon icon={faChevronCircleRight}></FontAwesomeIcon></button>);
 
-    return buttons;
   }
 
   return <div className="pagination">
