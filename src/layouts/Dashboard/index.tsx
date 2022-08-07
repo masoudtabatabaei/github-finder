7b3e1fc22fad093e4c5db212946272f0d586adc2
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-08-07 21:55:38 +0430

    Fix: Modify Dashboard as a general layout for project

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,91 +1,18 @@
-import React, { useCallback, useEffect, useState } from "react";
-import axios from "axios";
-import { IUserItem } from "../../types/users";
+import React, { ReactNode } from "react";
 import { Navbar } from "../../components/Navbar";
-import { NoResultFound } from "../../components/NoResultFound";
-import { Pagination } from "../../components/Pagination";
-import { Search } from "../../components/Search";
-import { Users } from "../../components/Users";
-import { Placeholder } from "../../components/Placeholder";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { About } from "../../components/About";
+import { Home } from "../../components/Home";
 
-export const Dashboard: React.FC = () => {
-  const [users, setUsers] = useState<IUserItem[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [showNoResultFound, setShowNoResultFound] = useState<boolean>(false);
-  const [viweMethod, setViewMethod] = useState<string>("grid");
 
-  const [totalItems , setTotalItems] = useState<number>(0);
-  const [pageSize , setPageSize] = useState<number>(20);
-  const [currentPage , setCurrentPage] = useState<number>(1);
-  const [mustBeResetPaginate , setMustBeResetPaginate] = useState<boolean>(false);
+interface IPanelProps {
+  children:ReactNode;
+}
 
-  const fetchUsers = useCallback(async () => {
-    if (!searchTerm) {
-      return;
-    }
-
-    try {
-      setIsLoading(true);
-      const result = await axios.get(
-        `https://api.github.com/search/users?q=${searchTerm}&in=name&type=user&per_page=${pageSize}&page=${currentPage}`
-      );
-      console.log("api: ", result.data.items);
-      result.data.total_count >= 1000 ?  setTotalItems(1000) : setTotalItems(result.data.total_count);
-      result.data.items.length > 0
-        ? setShowNoResultFound(false)
-        : setShowNoResultFound(true);
-      setUsers(result.data.items);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-      setMustBeResetPaginate(false);
-    }
-  }, [searchTerm , pageSize , currentPage]);
-
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
-
-  const handleSearch = (searchedTerm: string) => {
-    setSearchTerm(searchedTerm);
-    setMustBeResetPaginate(true);
-  };
-
-  const handleChangeView = (activeView: string) => {
-    console.log("active view: " + activeView);
-    setViewMethod(activeView);
-  };
-
+export const Dashboard: React.FC<IPanelProps> = ({children}) => {
   return (
-    <div>
-      <Navbar />
-      <div className="main_container">
-        <Search onSearch={handleSearch} view={viweMethod} changeView={handleChangeView} />
-        {isLoading ? (
-          <>
-            <Placeholder view={viweMethod} itemNumbers={pageSize}/>
-          </>
-        ) : users.length > 0 ? (
-          <Users users={users} viewMethod={viweMethod} />
-        ) : showNoResultFound ? (
-          <NoResultFound />
-        ) : (
-          <></>
-        )
-        }
-        {
-          users.length > 0 && 
-          <Pagination 
-            total={totalItems} 
-            pageSize={pageSize}
-            setPageSize={setPageSize}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-            mustBeReset={mustBeResetPaginate}/>
-        }
-      </div>
+    <div className="main_container">
+      { children }
     </div>
   );
 };
