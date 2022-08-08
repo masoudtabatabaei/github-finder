2709ef6dddcc2a278258be944894513b2c5f2141
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-08-08 16:59:53 +0430

    Fix: Modify About component

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import MyGithub from "../../assets/img/mygithub.png";
+import "./styles.css"
 
-export const  About:React.FC = () => {
-    return <>About Me</>;
+export const About: React.FC = () => {
+    return <div className="about_container">
+        <h2 className="title">About Github Finder</h2>
+        <p> 
+            Github Finder is an open source project that by use them you
+            can search a username that exist in github and view list of
+            users to see a list of users who have this username
+        </p>
+        <p>This project developed by <a href="https://github.com/masoudtabatabaei"><img src={MyGithub}/></a></p>
+    </div>;
 }
