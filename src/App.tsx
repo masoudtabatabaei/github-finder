995b2e104749caf25f47bb2e0fbdae5bf104ed2c
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-08-10 23:25:38 +0430

    Fix: Rename PanelRoutes to Routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { Dashboard } from "./layouts/Dashboard";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { Home } from "./components/Home";
-import { About } from "./components/About";
-import { PanelRoutes } from "./routes";
+import { Routes } from "./routes";
 import "./App.css";
 
 function App() {
-  return <PanelRoutes/>;
+  return <Routes/>;
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes as RoutesWrapper } from "react-router-dom";
 import { About } from "../components/About";
 import { Home } from "../components/Home";
 import { Navbar } from "../components/Navbar";
 import { Dashboard } from "../layouts/Dashboard";
 
-export const PanelRoutes = () => {
+export const Routes = () => {
   return (
     <Router>
       <Navbar />
       <Dashboard>
-        <Routes>
+        <RoutesWrapper>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
-        </Routes>
+        </RoutesWrapper>
       </Dashboard>
     </Router>
   );
