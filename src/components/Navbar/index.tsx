9a2e159b7b34ae29de93a1013bf9d1e716fb30a5
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-08-16 00:03:11 +0430

    Fix: Add setting icon link to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Setting from "../Setting";
 import "./styles.css";
 
 export const Navbar: React.FC = () => {
+  const [openSettingModal, setOpenSettingModal] = useState<boolean>(false);
+
   return (
-    <nav>
-      <div className="main_container header">
-        <div>
-          <Link to="/">Github Finder</Link>
+    <>
+      <nav>
+        <div className="main_container header">
+          <div>
+            <Link to="/">Github Finder</Link>
+          </div>
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/about" style={{ marginLeft: "30px" }}>About</Link>
+            <span className="setting" onClick={() => setOpenSettingModal(true)}><FontAwesomeIcon icon={faGear} fontSize="18px" color="#fff" /></span>
+          </div>
         </div>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/about" style={{ marginLeft: "30px" }}>About</Link>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      {openSettingModal && <Setting isOpen={openSettingModal} setIsOpen={setOpenSettingModal}/>}
+    </>
   );
 };
