86f3ac3f4465576ca7acda7e4ee40c8e1d08d8fc
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-08-30 21:17:06 +0430

    Feat: Create ConnectionAlert component to show connection alert message for more inform user

---
+++ src/components/ConnectionAlert/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+interface IConnectionAlertProps {
+  connectionState: boolean;
+}
+
+const ConnectionAlert: React.FC<IConnectionAlertProps> = ({ connectionState }) => {
+  const [hideSuccess , setHideSuccess] = useState<boolean>(false);
+  useEffect(() => {
+    if(connectionState) {
+      const timeOut = setTimeout(() => {
+        setHideSuccess(true);
+      }, 2000);
+
+      return () => clearTimeout(timeOut);
+    } else {
+      setHideSuccess(false);
+    }
+  },[connectionState]);
+
+  const renderAlertBox = () => {
+    let alertClassName = "offline";
+    let alertText = "Connection lost, Please try later!";;
+    if(connectionState) {
+      alertClassName = "online";
+      alertText = "Internet connection established";
+    }
+
+    return hideSuccess ? <></> : <div className={`connection_alert ${alertClassName}`}>{alertText}</div> ;
+  }
+
+  return renderAlertBox();
+}
+
+export default ConnectionAlert;
