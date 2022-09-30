d14be885761e418554a479e5e698ee3f7702f958
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-09-30 19:09:39 +0330

    Fix: Change styles of NotFoundResult component to styled-component

--- src/components/NoResultFound/index.tsx
+++ src/components/NoResultFound/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import noResultFoundIcon from "../../assets/img/no-result.png";
-import "./styles.css";
+import { NotFoundResult, NotFoundResultIcon } from "./styles";
 
 export const NoResultFound: React.FC = () => {
   return (
-    <div className="notFoundResult">
-      <img src={noResultFoundIcon} alt="not found result" />
-    </div>
+    <NotFoundResult>
+      <NotFoundResultIcon src={noResultFoundIcon} alt="not found result"/>
+    </NotFoundResult>
   );
 };
---
+++ src/components/NoResultFound/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NotFoundResult = styled.div`
+  text-align: center;
+  margin: 80px auto 10px;
+`;
+
+export const NotFoundResultIcon = styled.img`
+  max-width: 400px;
+  width: 100%;
+  height: auto;
+`;
