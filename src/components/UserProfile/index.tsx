93da718c0ca700d60653802122b53914abe9af85
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-10-17 23:54:32 +0330

    Feat: create base of fetchUserInfo method

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useCallback, useEffect } from "react";
 import Modal from "../../layouts/Modal";
 
 interface IUserProfileProps {
@@ -7,6 +8,20 @@ interface IUserProfileProps {
 }
 
 export const UserProfile:React.FC<IUserProfileProps> = ({isOpen, setIsOpen}) => {
+
+    const fetchUserInfo = async () => {
+        try {
+            const result = await axios.get("https://api.github.com/search/users/masoudtabatabaei");
+            console.log(result);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchUserInfo();
+    },[fetchUserInfo]);
+
     return <Modal title="User Profile" isOpen={isOpen} setIsOpen={setIsOpen}>
         User Profile
     </Modal>
