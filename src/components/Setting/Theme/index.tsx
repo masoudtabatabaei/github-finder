2cfef6103ba53dadf59bb575fb9f30118206a124
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-11-01 23:51:57 +0330

    Fix: Use useTheme hook in onclick the change Theme component

--- src/components/Setting/Theme/index.tsx
+++ src/components/Setting/Theme/index.tsx
@@ -1,20 +1,28 @@
 import React from "react";
+import useTheme from "../../../hooks/useTheme";
 import "./styles.css";
 
 const Theme:React.FC = () => {
+    const {theme, setMode} = useTheme();
+
+    const changeTheme = (themeName:string) => {
+        console.log("Theme:", themeName);
+        setMode(themeName);
+    }
+
     return <div className="theme_container">
-        <div className="theme_item_container">
+        <a className="theme_item_container" onClick={() => changeTheme("Classic")}>
             <div className="theme_item classic"></div>
             <p>Classic</p>
-        </div>
-        <div className="theme_item_container">
+        </a>
+        <a className="theme_item_container" onClick={() => changeTheme("Thinted")}>
             <div className="theme_item thinted"></div>
             <p>Thinted</p>
-        </div>
-        <div className="theme_item_container">
+        </a>
+        <a className="theme_item_container" onClick={() => changeTheme("Dark")}>
             <div className="theme_item dark"></div>
             <p>Dark</p>
-        </div>
+        </a>
     </div>
 }
 
