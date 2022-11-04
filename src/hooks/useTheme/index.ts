1f4b1f48c0c460c8e37f426de8c3f3856b723aa4
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-11-04 22:08:08 +0330

    Feat: Create useTheme hook for handle theme functionality

---
+++ src/hooks/useTheme/index.ts
@@ -0,0 +1,24 @@
+import {useState, useEffect} from "react";
+import StoreHelper from "../../helper/store-helper";
+import { IThemeType } from "../../theme.styled";
+import * as AllThemes from "../../theme.styled";
+
+
+const useTheme = () => {
+    const themes = StoreHelper.getFromLS("all-themes");
+    const [theme, setTheme] = useState(themes.Classic);
+
+    const setMode = (mode:IThemeType) => {
+        StoreHelper.setToLS("theme",mode);
+        setTheme(mode);
+    }
+
+    useEffect(() => {
+        const localTheme = StoreHelper.getFromLS("theme");
+        localTheme ? setTheme(localTheme) : setTheme(themes.Classic);
+    }, [theme]);
+
+    return {theme, setMode}
+};
+
+export default useTheme;
